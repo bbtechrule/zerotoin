74d19c46dfbfc1c27c17c6bc4a5226ff2c446e5f
bbtechrule <bbtechrule@gmail.com>
2026-01-24 12:16:54 +0530

    Add knowledge base for animal cell information

---
+++ data.js
@@ -0,0 +1,7 @@
+const knowledgeBase = [
+  {
+    keywords: ["animal cell", "animalcell"],
+    answer: "An animal cell is a eukaryotic cell with nucleus and organelles.",
+    image: "https://example.com/animal_cell.png"
+  }
+];
