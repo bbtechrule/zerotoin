0ab06425aa8f60a06e77f0267109f20265a353bd
bbtechrule <bbtechrule@gmail.com>
2026-01-24 12:38:22 +0530

    Initialize data.js with QA and diagrams
    Added QA and diagrams data structures for biology concepts.

---
+++ data.js
@@ -0,0 +1,12 @@
+const qa = {
+  "what is biotechnology": "Biotechnology is the use of living systems...",
+  "what is dna": "DNA is the hereditary material...",
+  "what is rna": "RNA helps in gene expression..."
+};
+
+const diagrams = {
+  "cell": "images/cell.png",
+  "dna": "images/dna.png",
+  "mitosis": "images/mitosis.png",
+  "meiosis": "images/meiosis.png"
+};
