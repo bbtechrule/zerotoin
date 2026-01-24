e3d9a97808f9cd51d1be40a15561b9d9c81c177e
bbtechrule <bbtechrule@gmail.com>
2026-01-24 12:20:41 +0530

    Delete data.js

--- data.js
+++
@@ -1,7 +0,0 @@
-const knowledgeBase = [
-  {
-    keywords: ["animal cell", "animalcell"],
-    answer: "An animal cell is a eukaryotic cell with nucleus and organelles.",
-    image: "https://example.com/animal_cell.png"
-  }
-];
